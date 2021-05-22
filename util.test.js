3da18441877af214322004f76d41864d30711152
foushware <mr.fouad992@gmail.com>
2021-05-22 19:40:23 +0200

    🛠️  integration Test

--- util.test.js
+++ util.test.js
@@ -1,5 +1,5 @@
 // This syntax used to testing ... i can use other import {} from ... syntax but let's focus on testing not setup
-const { generateText } = require('./util');
+const { generateText, checkAndGenerate } = require('./util');
 
 test('should output name and age', () => {
     const text = generateText("ahmed", 26);
@@ -12,4 +12,12 @@ test('should output data-less text', () => {
     const text = generateText('', null);
     expect(text).toBe(' (null years old)');
 
+})
+
+// * integration test it looks like unit test but it could fail due to checkAndGenerate() or the function called iniside it generateText()
+
+test('should generate a valid text output', () => {
+    const text = checkAndGenerate('foush', 26);
+    console.log(text);
+    expect(text).toBe('foush (26 years old)');
 })
